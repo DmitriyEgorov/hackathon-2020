483a91c5e75641e72c50822334d8d24aacf381cf
egorov <egorov@sevastopol.softlab.ru>
2019-06-03 17:36:20 +0300

    db_controller and integration test

--- hackathon-app/src/main/java/hackathon/controller/TestController.java
+++ hackathon-app/src/main/java/hackathon/controller/TestController.java
@@ -1,9 +1,13 @@
 package hackathon.controller;
 
+import hackathon.exception.TestDataEntityNotFoundException;
+import hackathon.model.TestData;
 import hackathon.model.TestResponse;
+import hackathon.processor.TestDataProcessor;
 import hackathon.processor.TestProcessor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.Produces;
@@ -19,19 +23,32 @@ import javax.ws.rs.core.MediaType;
 public class TestController {
 
     private final TestProcessor testProcessor;
+    private final TestDataProcessor testDataProcessor;
 
-    public TestController(TestProcessor testProcessor) {
+    public TestController(TestProcessor testProcessor, TestDataProcessor testDataProcessor) {
         this.testProcessor = testProcessor;
+        this.testDataProcessor = testDataProcessor;
     }
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String sayHi() {
         return testProcessor.getHiMessage();
     }
 
-    @RequestMapping("/ping")
+    @GetMapping(value = "/ping")
     @Produces(MediaType.APPLICATION_JSON)
     public ResponseEntity<TestResponse> ping() {
         return ResponseEntity.ok(testProcessor.ping());
     }
+
+    @GetMapping("/data/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseEntity<TestData> getTestData(@PathVariable("id") Long id) {
+        try {
+            return ResponseEntity.ok(testDataProcessor.findDataById(id));
+        } catch (TestDataEntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
--- hackathon-app/src/test/java/hackathon/controller/TestControllerIntegrationTest.java
+++ hackathon-app/src/test/java/hackathon/controller/TestControllerIntegrationTest.java
@@ -1,6 +1,8 @@
 package hackathon.controller;
 
 import hackathon.config.HackathonApplication;
+import hackathon.db.model.TestDataEntity;
+import hackathon.db.repository.TestDataEntityRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +32,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TestControllerIntegrationTest {
 
     @Autowired
+    private TestDataEntityRepository testDataEntityRepository;
+
+    @Autowired
     private MockMvc mvc;
 
     @Test
@@ -48,6 +53,35 @@ public class TestControllerIntegrationTest {
                 .andExpect(mvcResult ->
                         assertTrue(mvcResult.getResponse().getContentAsString().contains("test success")));
 
+    }
+
+    @Test
+    public void testData_success() throws Exception {
+        String test_data = "test_data";
+        TestDataEntity testDataEntity = new TestDataEntity();
+        testDataEntity.setData(test_data);
+        testDataEntity = testDataEntityRepository.save(testDataEntity);
+        mvc.perform(MockMvcRequestBuilders.get("/data/{id}", testDataEntity.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(mvcResult ->
+                        assertTrue(mvcResult.getResponse().getContentAsString().contains(test_data)));
+    }
+
+    @Test
+    public void testData_notFound() throws Exception {
+        testDataEntityRepository.deleteAll();
+        mvc.perform(MockMvcRequestBuilders.get("/data/{id}", Long.MAX_VALUE)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testData_badRequest() throws Exception {
+        testDataEntityRepository.deleteAll();
+        mvc.perform(MockMvcRequestBuilders.get("/data/{id}", "string")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 
 }
