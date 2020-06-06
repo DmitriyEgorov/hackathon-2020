a9627618440886534b91e742ac6afb937a513f95
egorov <egorov@sevastopol.softlab.ru>
2020-06-06 03:17:51 +0300

    Скрипты

--- build/scripts/competence_init.sql
+++ build/scripts/competence_init.sql
@@ -5,3 +5,27 @@ CREATE TABLE hackathon_test.competence (
 );
 
 INSERT INTO hackathon_test.competence (id, name) VALUES (1, 'Столяр');
+
+CREATE TABLE hackathon_test.competence_user (
+  ID         BIGSERIAL NOT NULL,
+  competence_id INTEGER NOT NULL,
+  USER_ID INTEGER NOT NULL,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE hackathon_test.competence_user
+  ADD CONSTRAINT userIdFk
+FOREIGN KEY (ID)
+REFERENCES hackathon_test.USERS;
+
+CREATE TABLE hackathon_test.competence_case (
+  ID         BIGSERIAL NOT NULL,
+  competence_id INTEGER NOT NULL,
+  CASE_ID INTEGER NOT NULL,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE hackathon_test.competence_case
+  ADD CONSTRAINT caseIdFk
+FOREIGN KEY (ID)
+REFERENCES hackathon_test.CASES;
