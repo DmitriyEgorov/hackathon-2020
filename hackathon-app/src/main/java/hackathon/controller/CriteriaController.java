1f54f595288201e23b9c7a7d4e1620ac58320d15
Albert <albert_lysenco@mail.ru>
2020-06-06 09:30:24 +0300

    Criteria controller fix

--- hackathon-app/src/main/java/hackathon/controller/CriteriaController.java
+++ hackathon-app/src/main/java/hackathon/controller/CriteriaController.java
@@ -2,6 +2,7 @@ package hackathon.controller;
 
 import hackathon.model.CriteriaRepresentation;
 import hackathon.services.CriteriaService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class CriteriaController {
 		this.criteriaService = criteriaService;
 	}
 
+	@GetMapping
 	public List<CriteriaRepresentation> getAllCriteria() {
 		return criteriaService.getAllCriteria();
 	}
